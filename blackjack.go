ed623d74f284c7deb6699b58c049e831871e088e
Kai T <kaithomas236@gmail.com>
2020-05-30 00:11:50 -0600

    blackjack is done!

--- blackjack.go
+++ blackjack.go
@@ -67,13 +67,27 @@ func getValue (card [2]string, dealer bool, prev int) (int) {
     return 10
   }else if card[0] == "Ace" {
     //this is a default for now but ideally I want
-    //if dealer:
-      //if prev hand val + 11 <=21:
-        //return 11
-      //else return 1
-    //if player:
-      //ask the player what they want, print their prev hand val
-    return 11
+    //fmt.Println(card)
+    if dealer == true {
+      if prev + 11 == 21 || prev + 11 < 19 {
+        return 11
+      } else {
+        return 1
+      }
+    }else {
+      var pChoice int
+      fmt.Printf("You have pulled an Ace. The value of your hand before this Ace is %v. Would you like this Ace to have a value of 11 (11) or 1 (1)?", prev)
+      fmt.Scan(&pChoice)
+      if pChoice == 11 {
+        return 11
+      }else if pChoice == 1 {
+        return 1
+      }else {
+        fmt.Println("Not a valid input. Value is 11.")
+        return 11
+      }
+    }
+
   }else {
     rank := card[0]
     val, err := strconv.Atoi(rank)
@@ -100,7 +114,7 @@ func main() {
   //fmt.Println(len(deck))
   var dHandVal int
   for i := 0; i < len(dHand); i++ {
-    dHandVal += getValue(dHand[i], false, 0)
+    dHandVal += getValue(dHand[i], true, 0)
   }
 
   var pHandVal int
@@ -119,50 +133,95 @@ func main() {
       fmt.Println("The dealer wins. The winning hand is", dHand, "and the value is 21. Try again!")
       os.Exit(1)
     }
-    //ask the player if they'd like to hit
-    pPrevVal := pHandVal
-    dPrevVal := dHandVal
-    pHit := true
-    var dHit bool
-    if dHandVal <= 18 {
-      dHit = true
-    }
-
-    if pHit == true {
-      fmt.Println("You drew", deck[len(deck)-1])
-      pHand = append(pHand, deck[len(deck)-1])//x = append(x,c...)
-      deck = updateDeck(deck)
-      pHandVal = 0
-      for i := 0; i < len(pHand); i++ {
-        pHandVal += getValue(pHand[i], false, pPrevVal)
-      }
-    }
-    if dHit == true {
-      dHand = append(dHand, deck[len(deck)-1])//x = append(x,c...)
-      deck = updateDeck(deck)
-      dHandVal = 0
-      for i := 0; i < len(dHand); i++ {
-        dHandVal += getValue(dHand[i], false, dPrevVal)
-      }
-    }
-
-    fmt.Println("Your current hand is", pHand, "and the value of your hand is", pHandVal)
-
     if dHandVal > 21 || pHandVal > 21 {
       if dHandVal > 21 && pHandVal > 21 {
-        fmt.Println("You both tie. Nobody wins.")
+        fmt.Println("You both bust. Nobody wins.")
         os.Exit(1)
       }else if pHandVal < 21 {
         fmt.Println("The dealer goes bust and you win. The losing hand is", dHand, "and the value is", dHandVal)
         os.Exit(1)
       }else if dHandVal < 21 {
-      fmt.Println("You go bust and the dealer wins. The losing hand is", pHand, "and the value is", pHandVal)
-      os.Exit(1)
+        fmt.Println("You go bust and the dealer wins. The losing hand is", pHand, "and the value is", pHandVal)
+        os.Exit(1)
       }
     }
 
+    //ask the player if they'd like to hit
+    var pInput string
+    fmt.Println("Would you like to hit (hit/h) or pass (pass/p)?")
+    fmt.Scan(&pInput)
+    var pHit bool
+
+    if pInput == "hit" || pInput == "h" {
+      pHit = true
+    }else if pInput == "pass" || pInput == "p" {
+      pHit = false
+    }else {
+      fmt.Println("Not a valid input. Passing.")
+      pHit = false
+    }
+
+    var dHit bool
+    if dHandVal <= 18 {
+      dHit = true
+    }
+
+    if dHit == false && pHit == false {
+      if pHandVal > dHandVal {
+        fmt.Println("You both decided to pass and you win! The value of your hand is", pHandVal, "and the value of the dealer's hand is", dHandVal)
+        os.Exit(1)
+      }else if pHandVal == dHandVal {
+        fmt.Println("You both decided to pass and you tied, nobody wins. The value of your hand is", pHandVal, "and the value of the dealer's hand is", dHandVal)
+        os.Exit(1)
+      }else {
+        fmt.Println("You both decided to pass and the dealer wins. The value of your hand is", pHandVal, "and the value of the dealer's hand is", dHandVal)
+        os.Exit(1)
+      }
+
+    }
+
+    pPrevVal := pHandVal
+    dPrevVal := dHandVal
 
 
+    if pHit == true {
+      fmt.Println("You drew", deck[len(deck)-1])
+      pHand = append(pHand, deck[len(deck)-1])//x = append(x,c...)
+      deck = updateDeck(deck)
+      pHandVal = pPrevVal + getValue(pHand[len(pHand)-1], false, pPrevVal)
+    }
+    if dHit == true {
+      dHand = append(dHand, deck[len(deck)-1])//x = append(x,c...)
+      deck = updateDeck(deck)
+      dHandVal = dPrevVal + getValue(dHand[len(dHand)-1], true, dPrevVal)
+    }
+
+    fmt.Println("Your current hand is", pHand, "and the value of your hand is", pHandVal)
+
+
+    if dHandVal > 21 || pHandVal > 21 {
+      if dHandVal > 21 && pHandVal > 21 {
+        fmt.Println("You both bust. Nobody wins.")
+        os.Exit(1)
+      }else if pHandVal < 21 {
+        fmt.Println("The dealer goes bust and you win. The losing hand is", dHand, "and the value is", dHandVal)
+        os.Exit(1)
+      }else if dHandVal < 21 {
+        fmt.Println("You go bust and the dealer wins. The losing hand is", pHand, "and the value is", pHandVal)
+        os.Exit(1)
+      }
+    }
   }
-
+  if dHandVal > 21 || pHandVal > 21 {
+    if dHandVal > 21 && pHandVal > 21 {
+      fmt.Println("You both bust. Nobody wins.")
+      os.Exit(1)
+    }else if pHandVal < 21 {
+      fmt.Println("The dealer goes bust and you win. The losing hand is", dHand, "and the value is", dHandVal)
+      os.Exit(1)
+    }else if dHandVal < 21 {
+      fmt.Println("You go bust and the dealer wins. The losing hand is", pHand, "and the value is", pHandVal)
+      os.Exit(1)
+    }
+  }
 }
